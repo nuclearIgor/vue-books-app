9fab172ce1bb73a28d0f17101628ca93357d80bd
nuclearIgor <igorflribeiro@gmail.com>
2022-12-11 21:16:14 -0300

    second commit

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -57,9 +57,9 @@ import Security from "@/components/security";
 import notie from 'notie'
 import {store} from "@/components/store";
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms))
+// }
 
 
 export default {
@@ -84,12 +84,12 @@ export default {
               text: res.message
             })
           } else {
-            sleep(1000).then(() => {
+            // sleep(1000).then(() => {
 
             this.users = res.data.users
             this.ready = true
             console.log(res.data)
-              })
+              // })
           }
         })
         .catch(error => console.log(error))
@@ -101,7 +101,7 @@ export default {
             text: "log this user out?",
             submitText: "Log out",
 
-            submitCallback:  () => {
+            submitCallback: () => {
               console.log("logging out user ", id)
               fetch(process.env.VUE_APP_API_URL + "/admin/log-user-out/" + id, Security.requestOptions(""))
                   .then(res => res.json())
@@ -109,6 +109,7 @@ export default {
                     if (data.error) {
                       // can't this.$emit here using the function(){} syntax because this being a callback it would refer to the window object
                       // es6 () => {} syntax makes "submitCallback inherit "this" and thus would work
+                      this.$emit
                       notie.alert({
                         type: "error",
                         text: data.message
